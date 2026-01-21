2754a46cbf715c8f90c4b22834a819953549a3ce
yapra <yapra@example.com>
2026-01-21 16:16:05 +0530

    Texture Test

--- src/components/Dog.jsx
+++ src/components/Dog.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import { useThree } from '@react-three/fiber'
 import { OrbitControls, useGLTF } from '@react-three/drei'
 
@@ -6,23 +6,28 @@ const Dog = () => {
   const model = useGLTF('/models/dog.drc.glb')
 
   useThree(({ camera }) => {
-    camera.position.z = 0.7
-    camera.position.y = 0.2
-    camera.position.x = 0
+    camera.position.set(0, 0.2, 1)
+  })
+
+  model.scene.traverse((child) => {
+    if (child.name.includes('DOG')) {
+      console.log(child.name)
+    }
   })
 
   return (
     <>
       <primitive
         object={model.scene}
-        position={[0.2, -0.57, 0]}
-        rotation={[-0.2, 0.7, 0]}
+        position={[0.2, -0.59, 0]}
+        rotation={[-0.3, 0.68, -0.009]}
         scale={1.1}
       />
 
+      <ambientLight intensity={0.5} />
       <directionalLight
         position={[2, 4, 5]}
-        intensity={10}
+        intensity={2}
       />
 
       <OrbitControls
@@ -33,4 +38,12 @@ const Dog = () => {
   )
 }
 
-export default Dog
+const DogWithSuspense = () => {
+  return (
+    <Suspense >
+      <Dog />
+    </Suspense>
+  )
+}
+
+export default DogWithSuspense
