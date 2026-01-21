be2c161cf99ad47f847166d782b0682ced8738b0
yapra <yapra@example.com>
2026-01-21 16:42:27 +0530

    Texture fix 1

--- src/components/Dog.jsx
+++ src/components/Dog.jsx
@@ -1,49 +1,50 @@
-import React, { Suspense } from 'react'
+import React, { useEffect } from 'react'
+import * as THREE from 'three'
 import { useThree } from '@react-three/fiber'
-import { OrbitControls, useGLTF } from '@react-three/drei'
+import { OrbitControls, useGLTF, useTexture } from '@react-three/drei'
 
 const Dog = () => {
   const model = useGLTF('/models/dog.drc.glb')
 
-  useThree(({ camera }) => {
-    camera.position.set(0, 0.2, 1)
+  // load textures from PUBLIC folder
+  const textures = useTexture({
+      normalMap: '/image.png',    // <-- use correct existing path
   })
 
-  model.scene.traverse((child) => {
-    if (child.name.includes('DOG')) {
-      console.log(child.name)
-    }
+  textures.normalMap.flipY = false // REQUIRED for normal maps
+  useThree(({ camera }) => {
+    camera.position.set(0, 0.4, 2.5)
   })
+
+  useEffect(() => {
+    model.scene.traverse((child) => {
+      if (child.isMesh) {
+        child.material.map = textures.map
+        child.material.normalMap = textures.normalMap
+
+        // REQUIRED for GLTF normal maps
+        child.material.normalMap.flipY = false
+
+        child.material.needsUpdate = true
+      }
+    })
+  }, [model, textures])
 
   return (
     <>
       <primitive
         object={model.scene}
-        position={[0.2, -0.59, 0]}
-        rotation={[-0.3, 0.68, -0.009]}
-        scale={1.1}
+        position={[0, -0.45, 0]}
+        rotation={[0, 0.7, 0]}
+        scale={1}
       />
 
-      <ambientLight intensity={0.5} />
-      <directionalLight
-        position={[2, 4, 5]}
-        intensity={2}
-      />
+      <ambientLight intensity={0.6} />
+      <directionalLight position={[4, 6, 5]} intensity={2.5} />
 
-      <OrbitControls
-        target={[0, 0, 0]}
-        enableDamping
-      />
+      <OrbitControls target={[0, 0, 0]} enableDamping />
     </>
   )
 }
 
-const DogWithSuspense = () => {
-  return (
-    <Suspense >
-      <Dog />
-    </Suspense>
-  )
-}
-
-export default DogWithSuspense
+export default Dog
